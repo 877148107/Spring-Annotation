5d9fd22ddf253933c65eb55167fbb3e289f99a0f
William.wang <William.wang@qq.com>
2019-11-18 22:54:49 +0800

    configuration:Spring的配置类

---
+++ src/main/java/com/spring/annotation/configuration/MyConfiguration.java
@@ -0,0 +1,10 @@
+package com.spring.annotation.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类=原来的xml配置文件
+ */
+@Configuration //告诉Spring这是一个配置类
+public class MyConfiguration {
+}
