4b300785d0637086a5da2109ef6ac8de714a40b9
William.wang <William.wang@qq.com>
2019-11-18 23:07:31 +0800

    configuration:Spring的配置类

--- src/main/java/com/spring/annotation/configuration/MyConfiguration.java
+++ src/main/java/com/spring/annotation/configuration/MyConfiguration.java
@@ -1,10 +1,17 @@
 package com.spring.annotation.configuration;
 
+import com.spring.annotation.bean.Person;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * 配置类=原来的xml配置文件
  */
-@Configuration //告诉Spring这是一个配置类
+@Configuration /**告诉Spring这是一个配置类**/
 public class MyConfiguration {
+
+    @Bean
+    public Person person(){
+        return new Person("张三",19);
+    }
 }
--- src/test/java/com/spring/annotation/AnnotationTest.java
+++ src/test/java/com/spring/annotation/AnnotationTest.java
@@ -1,8 +1,19 @@
 package com.spring.annotation;
 
+import com.spring.annotation.configuration.MyConfiguration;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AnnotationTest {
 
-    public static void main(String[] args) {
-
+    @Test
+    public void configuration(){
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(MyConfiguration.class);
+        Object person = applicationContext.getBean("person");
+        System.out.println(person);
+        String[] definitionNames = applicationContext.getBeanDefinitionNames();
+        for (String name : definitionNames) {
+            System.out.println(name);
+        }
     }
 }
